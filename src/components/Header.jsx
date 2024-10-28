cb3ad04f3adf3c11785da6ad465ffcc0e024f7fa
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-28 17:13:10 +0300

    header left ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+// imgs
+import logo from '../imgs/logo.svg'
+import menue from '../imgs/menu.svg'
+import search from '../imgs/search.svg'
 const Header = () => {
   return (
-    <div>Header</div>
+   <header className='py-4'>
+    <div className="containerb flex">
+      {/* left  */}
+      <div className='flex items-center'>
+        {/* logo */}
+        <Link className='mr-10' to='/'>
+           <img src={logo} alt="" />
+       </Link>
+       {/* katalog btn */}
+       <button className='bg-katalog flex items-center  p-2 rounded mr-4'>
+          <img className='mr-2' src={menue} alt="katalog" />
+          <p className='text-base text-white'>Каталог</p>
+       </button>
+       {/* input */}
+       <div className='relative'>
+        <input className='border border-katalog w-[375px] h-10 rounded pl-4' placeholder='Найти товар' type="search" />
+        <img className='absolute top-2 right-2' src={search} alt="" />
+       </div>
+      </div>
+    </div>
+   </header>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'katalog':'#70C05B',
+      }
+    },
   },
   plugins: [],
 }
