920182fa21576f78a029e90f1c832eede6b4e4dd
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-28 17:19:47 +0300

    add header full ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 // imgs
-import logo from '../imgs/logo.svg'
-import menue from '../imgs/menu.svg'
-import search from '../imgs/search.svg'
+import logo from "../imgs/logo.svg";
+import menue from "../imgs/menu.svg";
+import search from "../imgs/search.svg";
+import like from "../imgs/like.svg";
+import shop from "../imgs/shop.svg";
+import zakaz from "../imgs/zakaz.svg";
 const Header = () => {
   return (
-   <header className='py-4'>
-    <div className="containerb flex">
-      {/* left  */}
-      <div className='flex items-center'>
-        {/* logo */}
-        <Link className='mr-10' to='/'>
-           <img src={logo} alt="" />
-       </Link>
-       {/* katalog btn */}
-       <button className='bg-katalog flex items-center  p-2 rounded mr-4'>
-          <img className='mr-2' src={menue} alt="katalog" />
-          <p className='text-base text-white'>Каталог</p>
-       </button>
-       {/* input */}
-       <div className='relative'>
-        <input className='border border-katalog w-[375px] h-10 rounded pl-4' placeholder='Найти товар' type="search" />
-        <img className='absolute top-2 right-2' src={search} alt="" />
-       </div>
-      </div>
-    </div>
-   </header>
-  )
-}
+    <header className="py-4">
+      <div className="containerb flex justify-between">
+        {/* left  */}
+        <div className="flex items-center">
+          {/* logo */}
+          <Link className="mr-10" to="/">
+            <img src={logo} alt="" />
+          </Link>
+          {/* katalog btn */}
+          <button className="bg-katalog flex items-center  p-2 rounded mr-4">
+            <img className="mr-2" src={menue} alt="katalog" />
+            <p className="text-base text-white">Каталог</p>
+          </button>
+          {/* input */}
+          <div className="relative">
+            <input
+              className="border border-katalog w-[375px] h-10 rounded pl-4"
+              placeholder="Найти товар"
+              type="search"
+            />
+            <img className="absolute top-2 right-2" src={search} alt="" />
+          </div>
+        </div>
 
-export default Header
+        {/* right */}
+        <ul className="flex items-center space-x-6">
+          {/* like */}
+          <li>
+            <NavLink className='flex flex-col items-center'>
+              <img src={like} alt="like" />
+              <span>Избранное</span>
+            </NavLink>
+          </li>
+          {/* zakaz */}
+          <li>
+            <NavLink className='flex flex-col items-center' >
+              <img src={zakaz} alt="zakaz" />
+              <span>Заказы</span>
+            </NavLink>
+          </li>
+          {/* shop */}
+          <li>
+            <NavLink className='flex flex-col items-center'>
+              <img src={shop} alt="shop" />
+              <span>Корзина</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
