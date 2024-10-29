8089dbda98cce87170a9e09c6d93895f4f0c20d9
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-29 09:43:04 +0300

    add redux product wish list

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(<App />)
+// redux
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,22 @@ import Hero from '../components/Hero'
 import { products } from '../data'
 import { Link } from 'react-router-dom'
 
+import { addProductToWishlist } from '../store/slice/productsWishlistDataSlice';
+import { useDispatch } from "react-redux";
 // imgs
 import like from '../imgs/like.svg'
+
+
 const Home = () => {
   
+
+  const dispatch = useDispatch();
+
+  const mahsulotniSavatchagaQoshish = (product) => {
+    dispatch(addProductToWishlist(product));
+    console.log(product);
+  };
+
   
   return (
     <div >
@@ -18,7 +30,7 @@ const Home = () => {
             return(
               <li className='shadow-md relative p-2 rounded  h-[349px]' key={e.id}>
 
-                <button className='absolute right-2 top-2'>
+                <button className='absolute right-2 top-2 '>
                   <img src={like} alt="" />
                 </button>
 
@@ -40,7 +52,7 @@ const Home = () => {
                     <h3 className='text-base font-normal text-price mb-2'> {e.title}</h3>
                     <p>{e.yulduz}</p>
                   </div>
-                  <button className='text-base flex flex-col items-center w-full text-katalog border-2 border-katalog p-2 rounded'>В корзину</button>
+                  <button onClick={() => mahsulotniSavatchagaQoshish(e)} className='text-base flex flex-col items-center w-full text-katalog border-2 border-katalog p-2 rounded'>В корзину</button>
                 </Link>
               </li>
 
